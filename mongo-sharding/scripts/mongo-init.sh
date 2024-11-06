73597f831fb682ffd8af261df63b77f75a6f3567
pavell <pavel.hr@mail.ru>
2024-11-06 14:50:22 +0300

    docker compose with sharding and replicas.

--- mongo-sharding/scripts/mongo-init.sh
+++ mongo-sharding/scripts/mongo-init.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-###
-# Инициализируем бд
-###
-
-#docker compose exec -T mongodb1 mongosh <<EOF
-#use somedb
-#for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
-#EOF
-
-#docker compose exec -T shard1 mongosh --port 27018 --quiet <<EOF
-#use somedb
-#db.helloDoc.countDocuments()
-#EOF
-
-
-
 docker compose exec -T configSrv mongosh --port 27017 --quiet <<EOF
 rs.initiate(
   {
